b82015a61e70a7d8d0c1055de2e1cd327ced787f
Dahlia Guido <dguido.dev@gmail.com>
2023-08-11 18:57:07 -0500

    flooded seed.js with console logs to debug error

--- seeds/seed.js
+++ seeds/seed.js
@@ -14,18 +14,32 @@ const meetupPet = require("./meetupPetData.json");
 
 const seedDatabase = async () => {
   try {
+    console.log("Starting database seeding...");
     await sequelize.sync({ force: true });
+    console.log("Database synchronization completed.");
 
+    console.log("Seeding User data...");
     await User.bulkCreate(userData, {
       individualHooks: true,
       returning: true,
     });
+    console.log("User data seeded successfully.");
 
+    console.log("Seeding PetProfile data...");
     await PetProfile.bulkCreate(petProfileData);
+    console.log("PetProfile data seeded successfully.");
 
+    console.log("Seeding Meetup data...");
     await Meetup.bulkCreate(meetupData);
+    console.log("Meetup data seeded successfully.");
+    
+    console.log("Seeding MeetupComment data...");
     await MeetupComment.bulkCreate(meetupCommentData);
+    console.log("MeetupComment data seeded successfully.");
+
+    console.log("Seeding MeetupPet data...");
     await MeetupPet.bulkCreate(meetupPet);
+    console.log("MeetupPet data seeded successfully.");
 
     console.log("Database seeded successfully!");
     process.exit(0);
