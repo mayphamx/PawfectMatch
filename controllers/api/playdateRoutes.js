e48350b2fa8d2358cdbe1dd5fb20d3554d60ac4f
May <maypham@utexas.edu>
2023-08-12 23:42:25 -0500

    test

--- controllers/api/playdateRoutes.js
+++ controllers/api/playdateRoutes.js
@@ -39,25 +39,25 @@ router.post('/', withAuth, async (req, res) => {
 // UPDATE playdate by ID
 router.put('/:id', withAuth, async (req, res) => {
   try {
-    const affectedRowCount = await PlayDate.update(req.body, {
+    // const affectedRowCount = await PlayDate.update(req.body, {
+    //   where: {
+    //     id: req.params.id,
+    //     user_id: req.session.user_id,
+    //   },
+    // });
+
+    // if (affectedRowCount[0] === 0) {
+    //   return res.status(404).json({ message: 'Playdate not found!' });
+    // }
+
+    const updatedPlaydate = await PlayDate.update(req.body, {
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (affectedRowCount[0] === 0) {
-      return res.status(404).json({ message: 'Playdate not found!' });
-    }
-
-    const updatedPlaydate = await PlayDate.findOne({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    res.status(200).json({ playdateData: updatedPlaydate, message: 'Successfully updated playdate' });
+    res.status(200).json({ updatedPlaydate, message: 'Successfully updated playdate' });
   } catch (err) {
     res.status(500).json(err);
   }
